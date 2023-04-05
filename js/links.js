2a7b01767b957df9f72673926997c4dd33c0de54
gamingprophet <129402200+gamingprophet@users.noreply.github.com>
2023-04-06 03:33:43 +0500

    Add files via upload

---
+++ js/links.js
@@ -0,0 +1,17 @@
+
+document.getElementById("weekly").addEventListener("click", function() {
+    window.location.href = "#gotw";
+  });
+document.getElementById("low-end").addEventListener("click", function() {
+      window.location.href = "#gflep";
+ });
+document.getElementById("budget").addEventListener("click", function() {
+        window.location.href = "#gobp";
+      });
+document.getElementById("high-end").addEventListener("click", function() {
+          window.location.href = "#gfhep";
+        });
+        document.getElementById("#dtg ").addEventListener("click", function() {
+          window.location.href = "#downloadthisgames";
+        });
+
